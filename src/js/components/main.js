72cb763ab73f1f34e6d0245708b9c06ad314efce
Eduard Diumin <edwarddumin@gmail.com>
2024-03-21 17:27:32 +0200

    fix home slider

--- src/js/components/main.js
+++ src/js/components/main.js
@@ -1,12 +1,19 @@
-import Swiper, { Navigation, Pagination } from 'swiper';
-Swiper.use([Navigation, Pagination]);
+import Swiper, { Navigation, Pagination, Autoplay } from 'swiper';
+Swiper.use([Navigation, Pagination, Autoplay]);
 
 
 const swiper = new Swiper(".mainArticlesSwiper", {
   slidesPerView: 1,
   spaceBetween: 20,
+  loop: true,
+  speed: 2000,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: true
+  },
   pagination: {
     el: ".swiper-pagination",
+    clickable: true
   },
   breakpoints: {
     800: {
